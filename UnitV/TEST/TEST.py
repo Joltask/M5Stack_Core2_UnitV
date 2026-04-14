a901ef2e4fdc75bfbe6e023ef9b37b2b4f960872
Joltask <joltask@gmx.de>
2026-04-14 21:26:12 +0200

    Update TEST.py

--- UnitV/TEST/TEST.py
+++ UnitV/TEST/TEST.py
@@ -2,7 +2,6 @@ from machine import UART
 from fpioa_manager import fm
 import time
 
-# WICHTIG: Pins zuweisen!
 fm.register(34, fm.fpioa.UART2_TX)  # TX Pin
 fm.register(35, fm.fpioa.UART2_RX)  # RX Pin
 
